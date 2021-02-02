4bcfe3494a19548c83a709b1b35f7fcc1a550c67
atropos148 <tibor.h148@gmail.com>
2021-02-02 12:02:20 +0100

    Add competed code for SmallNim

--- src/SmallNim.java
+++ src/SmallNim.java
@@ -9,6 +9,17 @@ public class SmallNim {
         piles.put('B', 3);
         piles.put('C', 3);
 
-        System.out.printf("%s: %d %s: %d %s: %d", 'A', piles.get('A'), 'B', piles.get('B'), 'C', piles.get('C'));
+        while (piles.get('A') > 0 || piles.get('B') > 0 || piles.get('C') > 0) {
+            System.out.printf("%s: %d %s: %d %s: %d %n", 'A', piles.get('A'), 'B', piles.get('B'), 'C', piles.get('C'));
+            System.out.print("Choose a pile > ");
+            char chosenPile = Character.toUpperCase(keyboard.next().charAt(0));
+            System.out.printf("How many coins to remove from pile %s > ", chosenPile);
+            int coinsToRemove = keyboard.nextInt();
+            int newValue = piles.get(chosenPile) - coinsToRemove;
+            piles.put(chosenPile, newValue);
+        }
+        keyboard.close();
+        System.out.printf("%s: %d %s: %d %s: %d %n", 'A', piles.get('A'), 'B', piles.get('B'), 'C', piles.get('C'));
+        System.out.println("All piles are empty! Good job!");
     }
 }
