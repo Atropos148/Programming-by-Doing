c75504ba8f17d2092a9a5e2e601c14042f1b458e
atropos148 <tibor.h148@gmail.com>
2021-02-09 09:05:29 +0100

    Add digit counting function

--- src/NumberPuzzles.java
+++ src/NumberPuzzles.java
@@ -1,7 +1,18 @@
 public class NumberPuzzles {
     public static void main(String[] args) {
+        digitTotals();
+    }
+
+    static void digitTotals() {
         for (int i = 10; i <= 56; i++) {
-            System.out.println(i);
+            int digitTotal = 0;
+            for (int j = 0; j < String.valueOf(i).length(); j++) {
+                int digit = Character.getNumericValue(String.valueOf(i).charAt(j));
+                digitTotal += digit;
+            }
+            if (digitTotal > 10) {
+                System.out.println(i);
+            }
         }
     }
 }
