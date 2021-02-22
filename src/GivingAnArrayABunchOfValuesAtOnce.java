581ef019da29be2f45f6fdd23e98b9b2fb693d45
atropos148 <tibor.h148@gmail.com>
2021-02-22 10:04:22 +0100

    Add solution for GivingAnArrayABunchOfValues

--- src/GivingAnArrayABunchOfValuesAtOnce.java
+++ src/GivingAnArrayABunchOfValuesAtOnce.java
@@ -1,11 +1,18 @@
 public class GivingAnArrayABunchOfValuesAtOnce {
     public static void main(String[] args) {
-        String[] arr1 = { "alpha", "bravo", "charlie" };
+        String[] arr1 = { "alpha", "bravo", "charlie", "delta", "echo" };
 
-        System.out.print("The first array is filled with the following values:\n\t");
+        System.out.printf("The first array is filled with the following values:%n  ");
         for (int i = 0; i < arr1.length; i++)
             System.out.print(arr1[i] + " ");
         System.out.println();
 
+        int[] arr2 = { 12, 24, 36, 48, 60 };
+
+        System.out.printf("The second array is filled with the following values:%n  ");
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+        System.out.println();
     }
 }
