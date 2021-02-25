cecf6c0bf8da9318cb3d48299889356ec628b073
atropos148 <tibor.h148@gmail.com>
2021-02-25 12:41:46 +0100

    Add code for Address and AdressString

--- src/WebAddressesArray.java
+++ src/WebAddressesArray.java
@@ -1,65 +1,84 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-class Address {
+class AddressString {
     String street;
     String city;
     String state;
     int zip;
+
+    public String oneLineAdress() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(this.street + ", ");
+        sb.append(this.city + ", ");
+        sb.append(this.state + ", ");
+        sb.append(this.zip);
+
+        return sb.toString();
+    }
 }
 
-public class WebAddresses {
+public class WebAddressesArray {
     public static void main(String[] args) throws Exception {
         File file = new File("adresses.txt");
+        ArrayList<AddressString> addresses = new ArrayList<>();
 
         try (Scanner fin = new Scanner(file)) {
 
-            Address uno = new Address();
+            AddressString uno = new AddressString();
             uno.street = fin.nextLine();
             uno.city = fin.nextLine();
             uno.state = fin.next();
             uno.zip = fin.nextInt();
-            System.out.println(uno.street + ", " + uno.city + ", " + uno.state + " " + uno.zip);
+            addresses.add(uno);
 
             // skip ghost newline
-            System.out.println(fin.nextLine());
+            fin.nextLine();
 
-            Address dos = new Address();
+            AddressString dos = new AddressString();
             dos.street = fin.nextLine();
             dos.city = fin.nextLine();
             dos.state = fin.next();
             dos.zip = fin.nextInt();
-            System.out.println(dos.street + ", " + dos.city + ", " + dos.state + " " + dos.zip);
+            addresses.add(dos);
 
             // skip ghost newline
-            System.out.println(fin.nextLine());
+            fin.nextLine();
 
-            Address tre = new Address();
+            AddressString tre = new AddressString();
             tre.street = fin.nextLine();
             tre.city = fin.nextLine();
             tre.state = fin.next();
             tre.zip = fin.nextInt();
-            System.out.println(tre.street + ", " + tre.city + ", " + tre.state + " " + tre.zip);
+            addresses.add(tre);
 
             // skip ghost newline
-            System.out.println(fin.nextLine());
+            fin.nextLine();
 
-            Address four = new Address();
+            AddressString four = new AddressString();
             four.street = fin.nextLine();
             four.city = fin.nextLine();
             four.state = fin.next();
             four.zip = fin.nextInt();
-            System.out.println(four.street + ", " + four.city + ", " + four.state + " " + four.zip);
+            addresses.add(four);
 
             // skip ghost newline
-            System.out.println(fin.nextLine());
+            fin.nextLine();
 
-            Address five = new Address();
+            AddressString five = new AddressString();
             five.street = fin.nextLine();
             five.city = fin.nextLine();
             five.state = fin.next();
             five.zip = fin.nextInt();
-            System.out.println(five.street + ", " + five.city + ", " + five.state + " " + five.zip);
+            addresses.add(five);
+
+            int index = 1;
+            for (AddressString adress : addresses) {
+                System.out.println(index + ": " + adress.oneLineAdress());
+                index++;
+            }
         }
     }
 }
